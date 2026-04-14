18b2a31e6496479dd8867f9fe92ce598ac8a0179
Cedric <crubbertire@rogers.com>
2026-04-14 14:04:24 -0700

    Formatting strings in go

--- variables.go
+++ variables.go
@@ -2,11 +2,23 @@ package main
 
 import "fmt"
 
+func stringInterpolation() {
+	const name = "Cedric"
+	const openRate = 5.2
+
+	/*
+		For string interpolation:
+		- %v any value in default format ("i am %v years old", 23 OR "too many")
+		- %s string values ("i am %s years old", "too many")
+		- %d integer values ("i am %d years old", 23)
+		- %f float values ("i am %.2f years old", 23.756)
+			- %.2f denotes decimal points.
+	*/
+	msg := fmt.Sprintf("Hi %s, your open rate is %.1f percent\n", name, openRate)
+
+	fmt.Print(msg)
+}
+
 func main() {
-	const fName = "Cedric"
-	const lName = "Robitaille"
-
-	s := fmt.Sprintf("Hello %s %s\n", fName, lName)
-
-	fmt.Print(s)
+	stringInterpolation()
 }
